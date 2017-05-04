9e5e73f4dc6c50395c89443a920f6496d067e142
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-05-04 21:09:01 +0600

    Minor UI fixes

--- src/components/ContentContainer/Disconnected/index.js
+++ src/components/ContentContainer/Disconnected/index.js
@@ -3,11 +3,8 @@ import styles from './styles.css';
 
 const Disconnected = ({ state }) =>
     <div className={styles.component}>
-        Oops-a-daisy!&nbsp;😕&nbsp;&nbsp;&nbsp;I have troubles with connection to server.<br />
-        Check your internet connection or drop few lines about that to my creator&nbsp;
-        <a className={styles.link} href="https://twitter.com/vladimore" target="_blank">
-            @vladimore
-        </a>
+        Oops-a-daisy!&nbsp;😕&nbsp;&nbsp;&nbsp;Some troubles with connection to server.<br />
+        Check your internet connection or drop few lines at catindev@gmail.com
     </div>
     ;
 
